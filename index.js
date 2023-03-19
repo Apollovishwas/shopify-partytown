a4d83fb9adbe38dfb79ad039849ad1758c9a18cb
Vishwas Chandran <Apollovishwas@yahoo.com>
2023-03-19 19:47:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use('/proxy', express.static('./proxy'));
 
 app.get('/', (req, res) => {
-  res.send('Hello PartyTown!');
+  res.send('Hello PartyTown 2.0');
 });
 
 app.use('/reverse-proxy', async (req, res) => {
